6efd4ec5f28d2541d4ca9c7f7e9a4bbb1394a3e1
Yuragranovsky <yuragranovsky@gmail.com>
2022-03-31 18:37:59 +0300

    final code hw#32

--- src/index.js
+++ src/index.js
@@ -1,42 +1,8 @@
-// let str:string;
-// str = "10";
-// function fun(op1: number, op2: number): number {
-//     return op1 * op2;
-// }
-// let a = fun(2, 2);
-// a = 10;
-// let b = 10;
-// let d = b - +"10";
-// if(str) {
-//     console.log(str);
-// }
-// function fun1(s:string): boolean {
-//     return !!s;
-// }
-// let vf: (n1:number, n2: number)=>string;
-// function fun4(op1: number, op2:number):string {
-//  return op1 + op2 + "";
-// }
-// vf = fun4;
-// console.log(vf(2, 2))
-function sum(ar) {
-    return ar.reduce((res, cur) => res + cur);
-}
-// let ar: Array<number> = [1 ,2 ,3, 10];
-// ar.push(12);
-// console.log(sum(ar));
-//let set = new Set<number>([1,1,1,2,2,3,3]);
-//console.log(set)
-//set.forEach(e => console.log(e));
-// set.add(10);
-// const ar1 = Array.from(set);
-// console.log(set.has(10));
-// console.log(sum(ar1))
 /*****************************************HW #32 */
 function intersection(set1, set2) {
     //function returning array of common numbers between two sets
     //that is the numbers existing in both sets
-    return Array.from(set1).filter(n => set2.has(n));
+    return Array.from(set1).filter(Set.prototype.has.bind(set2));
 }
 function sbtract(set1, set2) {
     //write function returning array of numbers from set1 that don't exist in the 
@@ -61,40 +27,6 @@ function getSortedOccurrences(array) {
         .sort((o1, o2) => o2.count - o1.count || o1.str.localeCompare(o2.str));
 }
 /**************************************************************************** */
-// const mapMonths = new Map<number, string>([
-//     [1, "Januar"], [2, "Februar"], [3,'March']
-// ]);
-// mapMonths.set(4, 'April');
-// mapMonths.set(1, "Jan");
-// // console.log(mapMonths);
-// // console.log(mapMonths.get(5));
-// console.log(mapMonths.has(5));
-// const arEntries = Array.from(mapMonths); //array of entries
-// console.log(arEntries);
-// const arKeys = Array.from(mapMonths.keys()); //array of kyes
-// arEntries.push([6, 'hhhh']);
-// arKeys.push(10);
-// console.log(arKeys);
-// const arValues = Array.from(mapMonths.values());
-// console.log(arValues);
-// arValues.push("10");
-// type Person = {
-//     id:number,
-//     name: string,
-//     age?: number,
-//     city?: string
-// }
-// function createPerson(id: number, name: string): Person {
-//     return {id, name};
-// }
-// console.log(createPerson(123, "Vasya"))
-// let person: Person = {id: 100, name: "Moshe", age: 20, city: 'Lod'}
-// let persons: Person[] = []
-// persons.push(person);
-// function strLength(str:string): number {
-//     return str.length;
-// }
-// console.log(strLength(person.city));
 Set.prototype.toString = function () { return Array.from(this).toString(); };
 const set1 = new Set([1, 2, 3, 4, 5]);
 const set2 = new Set([2, 5, 7, -10]);
